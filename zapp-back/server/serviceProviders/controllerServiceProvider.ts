0fe7662d5f0909e3a8a82962445bae323f512c83
Abdallah Gamal <abdoolly@gmail.com>
2018-10-16 15:42:43 +0200

    init

---
+++ zapp-back/server/serviceProviders/controllerServiceProvider.ts
@@ -0,0 +1,9 @@
+/**
+ * make the controllers with lazy loading which means that
+ * means when we need a controller we load it in the service provider
+ * 
+ */
+export class controllerProvider {
+
+}
+
